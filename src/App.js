dc233583b8fdce25c13bc5d438c4d0ab8724a52f
Jesús Barros <jesus.barros.castro@gmail.com>
2018-10-24 18:28:10 +0200

    Añado Semantic UI React como framework de componentes visuales para la IU

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from "./components/Home";
 import DetailsPodcast from "./components/DetailsPodcast";
 import DetailsEpisode from "./components/DetailsEpisode";
 
-const App = () => <div>
-  <Route path="/" exact component={Home} />
-  <Route path="/podcast/:podcastId" exact component={DetailsPodcast} />
-  <Route path="/podcast/:podcastId/episodes/:episodeId" component={DetailsEpisode} />
-</div>;
+const App = () => (
+  <div className="ui container">
+    <Route path="/" exact component={Home} />
+    <Route path="/podcast/:podcastId" exact component={DetailsPodcast} />
+    <Route path="/podcast/:podcastId/episodes/:episodeId" component={DetailsEpisode} />
+  </div>
+);
 
 export default App;
